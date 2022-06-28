dfc1af6093643f3131a385803c3ea4088cd2447e
FinXCodeX <108317178+FinXCodeX@users.noreply.github.com>
2022-06-28 06:56:00 +0300

    Add files via upload

---
+++ Modül kurulumu.bat
@@ -0,0 +1,62 @@
+@echo off
+color b
+cd C:\Users\%username%\AppData\Local\Programs\Python\Python310\Scripts
+color c
+pip install
+color a
+pip install aiohttp          
+color b         
+pip install altgraph      
+color c            
+pip install async-timeout             
+pip install attrs          
+color b           
+pip install certifi   
+color c                
+pip install cffi                      
+pip install chardet                   
+pip install charset-normalizer        
+pip install colorama                  
+pip install colour                    
+pip install discord                   
+pip install discord.py             
+pip install emoji             
+pip install future                    
+pip install gevent                    
+pip install greenlet                  
+pip install idna                      
+pip install keyboard                  
+pip install MouseInfo                 
+pip install multidict                 
+pip install pefile                    
+pip install pip                       
+pip install psutil                    
+pip install PyAutoGUI                 
+pip install pycparser                 
+pip install PyGetWindow               
+pip install pyinstaller               
+pip install pyinstaller-hooks-contrib 
+pip install PyMsgBox                  
+pip install PyNaCl                    
+pip install pyperclip                 
+pip install PyRect                    
+pip install PyScreeze                 
+pip install python-dotenv             
+pip install pytweening                
+pip install pywin32-ctypes            
+pip install requests                  
+pip install setuptools          
+pip install tqdm          
+pip install typing_extensions       
+pip install urllib3        
+pip install webdriver-manager       
+pip install websocket                 
+pip install yarl                      
+pip install zope.event         
+pip install zope.interface  
+clear
+message * Kurulum tamam..
+timeout /t 2
+echo Modüller kurulmuş gibi gözüküyor program kapaniyor...
+timeout /t 5
+close
